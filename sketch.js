f10b7b61c6b35dcd692782f582325568724cc7a4
sidhgoyal <sidhgoyal>
2020-10-09 14:24:08 +0530

    Bullets and Walls

--- sketch.js
+++ sketch.js
@@ -1,40 +1,49 @@
-var car, wall;
+var bullet, wall;
 var speed, weight;
+var thickness;
 
 function setup() {
 
   createCanvas(1600,400);
   
-  speed = random(55,90);
-  weight = random(400,1500)
-  car = createSprite(50,200,50,50);
-  wall = createSprite(1500,200,60,height/2)
-  car.velocityX = speed;
+  thickness = random(22,83);
+  speed = random(223,321,);
+  weight = random(30,52)
+  bullet = createSprite(50,200,50,50);
+  wall = createSprite(1200,200,thickness,height/2)
+  bullet.velocityX = speed;
 }
 
 function draw() {
   background(0); 
   
+
   
 
-  if(wall.x-car.x < (car.width-wall.width)/2)
-{
- car.velocityX=0;
-var deformation = 0.5 * weight * speed * speed/22509;
-if(deformation>180)
-{
- car.shapeColor = color("red");
-}
-if(deformation < 180 && deformation>100)
-{
- car.shapeColor = color("yellow");
-}
-if(deformation<100)
-{
- car.shapeColor=color("green");
-}
-}
+  
 
+  if(hasCollided(bullet, wall)){
+    bullet.velocityX=0;
+    var damage=0.5* weight*speed*speed/ (thickness*thickness*thickness);
+  }
+    if(damage>10){
+    wall.shapeColor=color(255,0,0)
+    }
+    if(damage<10){
+    wall.shapeColor=color(0,255,0);
+    
+    }
+  
+    drawSprites();
 
-  drawSprites();
 }
+function hasCollided(lbullet, lwall){
+  bulletRightEdge=lbullet.x +lbullet.width;
+  wallLeftEdge=lwall.x;
+  
+  if (bulletRightEdge>=wallLeftEdge) {
+    return true;
+  }
+  return false;
+  
+  }
